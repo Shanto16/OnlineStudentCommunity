1b253bccbeb630683249231c74cce821a896d61b
Shanto16 <shakil.glostars@gmail.com>
2018-08-31 02:50:24 +0600

    fixes

---
+++ app/src/main/java/nsublack/onlinestudentcommunity/FeedDataModel.java
@@ -0,0 +1,63 @@
+package nsublack.onlinestudentcommunity;
+
+public class FeedDataModel  {
+
+    private int imageID;
+    private String nameID,varsityID,captionID,subjectID,followingID,commentsID;
+
+    public int getImageID() {
+        return imageID;
+    }
+
+    public void setImageID(int imageID) {
+        this.imageID = imageID;
+    }
+
+    public String getNameID() {
+        return nameID;
+    }
+
+    public void setNameID(String nameID) {
+        this.nameID = nameID;
+    }
+
+    public String getVarsityID() {
+        return varsityID;
+    }
+
+    public void setVarsityID(String varsityID) {
+        this.varsityID = varsityID;
+    }
+
+    public String getCaptionID() {
+        return captionID;
+    }
+
+    public void setCaptionID(String captionID) {
+        this.captionID = captionID;
+    }
+
+    public String getSubjectID() {
+        return subjectID;
+    }
+
+    public void setSubjectID(String subjectID) {
+        this.subjectID = subjectID;
+    }
+
+    public String getFollowingID() {
+        return followingID;
+    }
+
+    public void setFollowingID(String followingID) {
+        this.followingID = followingID;
+    }
+
+    public String getCommentsID() {
+        return commentsID;
+    }
+
+    public void setCommentsID(String commentsID) {
+        this.commentsID = commentsID;
+    }
+}
